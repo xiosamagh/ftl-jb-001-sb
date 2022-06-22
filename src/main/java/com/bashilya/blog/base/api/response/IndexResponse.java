e449b81160cedb4bfb257fb0706691f6aaed32b5
Ilya <92110133+xiosamagh@users.noreply.github.com>
2022-06-22 15:53:10 +0300

    L001 - HelloRestApiController

---
+++ src/main/java/com/bashilya/blog/base/api/response/IndexResponse.java
@@ -0,0 +1,14 @@
+package com.bashilya.blog.base.api.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class IndexResponse {
+
+    private String message;
+
+}
